d4692df729f19955340d205c8cbfd6679dd5a488
Rrrickyy <109094954+Rrrickyy@users.noreply.github.com>
2022-07-13 11:52:28 +0200

    bus problem

--- Repositoryy-1/ConsoleApplication2/ConsoleApplication2.cpp
+++ Repositoryy-1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,12 +1,18 @@
 
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    auto x = 25 % 7;
-    cout << x;
+int main() {
+    //your code goes here
+    int a;
+    cin >> a;
+    int bus = a % 50;
+    int c = 50 - bus;
+    cout << c;
+
     return 0;
 }
+
 
 
