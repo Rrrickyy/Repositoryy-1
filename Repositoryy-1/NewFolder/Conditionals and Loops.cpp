a736ba209f377c09c028ad50cec8e5d29117dc8d
Rrrickyy <109094954+Rrrickyy@users.noreply.github.com>
2022-07-18 10:32:38 +0200

    Kalkulator

---
+++ Repositoryy-1/NewFolder/Conditionals and Loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int	main()
+{
+	double czynnik1, czynnik2;
+	int znak;
+	cin >> czynnik1; 
+	cout << czynnik1;
+	cin >> znak;
+	cout << znak;
+	cin >> czynnik2;
+	cout << czynnik2;
+
+}
